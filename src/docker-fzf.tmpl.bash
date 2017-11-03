571c116abeefa54f6bf046db1a1e6f36484feb9c
Michał Nowotnik <m.nowot@gmail.com>
2017-11-04 00:21:23 +0100

    Change tabs to spaces

--- src/docker-fzf.tmpl.bash
+++ src/docker-fzf.tmpl.bash
@@ -3,22 +3,22 @@
 #{{content}}
 
 _fzf_complete_docker() {
-	local cur prev words cword
-	_get_comp_words_by_ref -n : cur prev words cword
+    local cur prev words cword
+    _get_comp_words_by_ref -n : cur prev words cword
 
     if ! type _docker > /dev/null 2>&1; then
         _completion_loader "$@"
         complete -F _fzf_complete_docker -o default -o bashdefault docker
     fi
 
-	local counter=1
-	while [ $counter -lt $cword ]; do
-		case "${words[$counter]}" in
-			run)
+    local counter=1
+    while [ $counter -lt $cword ]; do
+        case "${words[$counter]}" in
+            run)
                 _fzf_complete_docker_run "$@"
                 return
             ;;
-			exec|rm)
+            exec|rm)
                 _fzf_complete_docker_container "$@"
                 return
             ;;
@@ -26,9 +26,9 @@ _fzf_complete_docker() {
                 _fzf_complete_docker_common "$@"
                 return
             ;;
-		esac
-		(( counter++ ))
-	done
+        esac
+        (( counter++ ))
+    done
     _fzf_handle_dynamic_completion docker "$@"
 }
 
